bea51bc8f238651cf21acf3dc3f5f46bcf332bdb
21336762 <21336762@dut4life.ac.za>
2016-03-21 12:15:48 +0200

    Main

---
+++ ZA_Khan/ZA_Khan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZA_Khan.MvcApplication" Language="C#" %>
---
+++ ZA_Khan/ZA_Khan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZA_Khan.Startup))]
+namespace ZA_Khan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
